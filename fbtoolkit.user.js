36bc6636f2e6cb58f79be4e6c40481ce1152a3df
Chris <rootdev4@gmail.com>
2020-03-17 13:28:51 +0100

    Adding friendlist extracting functionality

--- fbtoolkit.user.js
+++ fbtoolkit.user.js
@@ -70,11 +70,10 @@ function showUserId() {
 function scrollTimeline() {
     try {
         toggleImg()
-        let task = null
-        task = setInterval(() => {
+        let loadTimeline = setInterval(() => {
             window.scrollBy(0, document.body.scrollHeight)
             if (document.querySelector('i.img.sp_jgaSVtiDmn_.sx_fa8e49')) {
-                clearInterval(task)
+                clearInterval(loadTimeline)
                 window.scrollBy(0, document.body.scrollHeight)
                 window.scrollTo(0, 0)
                 toggleImg()
@@ -107,6 +106,113 @@ function expandTimeline() {
 }
 
 /**
+ * Print out an user's friendlist as CSV formatted string
+ * @param {*} fList Array with user's friends/followers
+ */
+function printList(fList) {
+    try {
+        const username = document.querySelector('span#fb-timeline-cover-name').textContent
+        let output = `<html><head><title>${username}</title></head><body>`
+        output += `<h1>${fList[0]} list of ${username}</h1><p>CSV formatted list of public ${fList[0].toLowerCase()}. `
+        output += `Extracted ${fList.length - 1} visible ${fList[0].toLowerCase()} at all.</p><hr>`
+        fList.shift()
+        fList.forEach(user => output += `${[user.id, user.vanity, user.name, user.image].join()}<br>`)
+        document.write(`${output}</body></html>`)
+    } catch (exception) {
+        alertError(exception, 'Printing list failed.')
+    }
+}
+
+/**
+ * Extract Facebook vanity username out of user's profile URL
+ * @param {*} userUrl User's profile URL
+ */
+function getVanityName(userUrl) {
+    try {
+        const name = /facebook.com\/(.*?)\?/g.exec(userUrl)[1] || null
+        if (name !== null && !name.contains('profile.php')) return name
+        throw new Exception()
+    } catch (exception) {
+        return null
+    }
+}
+
+/**
+ * Extract an user's data out of HTML user card node
+ * @param {*} userCard HTML user card node
+ */
+function getUserData(userCard) {
+    try {
+        const userId = /id=(.*?)\&/g.exec(userCard.getAttribute('data-hovercard'))[1] || null
+        const vanityName = getVanityName(userCard.getAttribute('href'))
+        const userName = userCard.querySelector('img:first-child').getAttribute('aria-label') || null
+        const userImage = userCard.querySelector('img:first-child').getAttribute('src') || null
+
+        return {'id': userId, 'vanity': vanityName, 'name': userName, 'image': userImage}
+    } catch (exception) {
+        console.error(`Parsing user card for ${userCard} failed with error ${exception}.`)
+        return {}
+    }
+}
+
+/**
+ * Extract an user's friend-/followerlist
+ */
+function extractFriendlist() {
+    try {
+        const followersList = document.querySelector('div#pagelet_collections_followers') || null
+        const friendsList = document.querySelector('div#pagelet_timeline_medley_friends') || null
+
+        // Followers
+        if (followersList !== null) {
+            toggleImg()
+            let loadFollowersList = setInterval(() => {
+                window.scrollBy(0, document.body.scrollHeight)
+
+                if (document.querySelector('div.morePager') === null) {
+                    clearInterval(loadFollowersList)
+                    window.scrollBy(0, document.body.scrollHeight)
+                    window.scrollTo(0, 0)
+
+                    let list = ['Followers']
+                    followersList.querySelectorAll('ul.uiList > li.fbProfileBrowserListItem > div > a').forEach(follower => {
+                        list.push(getUserData(follower))
+                    })
+                    return printList(list)
+                }
+            }, 100)
+        }
+        
+        // Friends
+        else if (friendsList !== null) {
+            toggleImg()
+            let loadFriendsList = setInterval(() => {
+                window.scrollBy(0, document.body.scrollHeight)
+
+                if (document.querySelectorAll('div#pagelet_timeline_medley_photos').length) {
+                    clearInterval(loadFriendsList)
+                    window.scrollBy(0, document.body.scrollHeight)
+                    window.scrollTo(0, 0)
+
+                    let list = ['Friends']
+                    friendsList.querySelectorAll('ul.uiList > li > div[data-testid="friend_list_item"] > a').forEach(friend => {
+                        list.push(getUserData(friend))
+                    })
+                    return printList(list)
+                }
+            }, 100)
+        }
+
+        else {
+            return alertError('Friendlist not found.', 'You\'re not showing an user\'s friend- or followerslist.')
+        }
+    } catch (exception) {
+        alertError(exception, 'Extracting the user\'s friendlist failed.')
+    }
+}
+
+
+/**
  * Hide an element from DOM
  * @param {*} cssSelector DOM node identified by CSS selector
  */
@@ -114,10 +220,12 @@ function hide(cssSelector) {
     try {
         let selector = cssSelector
         let pNode = false
+
         if (selector.includes(':parent')) {
             pNode = true
             selector = selector.replace(':parent', '')
         }
+
         document.querySelectorAll(selector).forEach(element => {
             if (element != null) {
                 if (pNode) element = element.parentNode
@@ -141,6 +249,10 @@ function clearTimeline() {
     hide('div#timeline_sticky_header_container')
     hide('ul[data-referrer="timeline_light_nav_top"]')
     hide('div#pagelet_escape_hatch') // Follow profile
+    hide('a[href="/subscriptions/suggestions/"]') // "Find People to Follow" link
+    hide('a[ajaxify*="/ajax/follow/follow_profile.php"]') // Follow button in followers list
+    hide('a[href="/find-friends/browser/"]:parent') // "Friend Requests / Find Friends" buttons
+    hide('button[aria-label="Manage"]:parent') // Manage sections button
     hide('div#pagelet_pymk_timeline')
     hide('div#pagelet_timeline_composer')
     // Add more elements here...
@@ -169,6 +281,7 @@ async function init() {
                             <li style="padding: 3px"><a href="#" id="fbToolkitScroll" class="fbToolkitLink">Scroll user timeline</a></li>
                             <li style="padding: 3px"><a href="#" id="fbToolkitExpand" class="fbToolkitLink">Expand hidden content</a></li>
                             <li style="padding: 3px"><a href="#" id="fbToolkitClear" class="fbToolkitLink">Clear user profile</a></li>
+                            <li style="padding: 3px"><a href="#" id="fbToolkitFriends" class="fbToolkitLink">Extract user's friendlist</a></li>
                             <hr>
                             <li style="padding: 3px"><a href="#" class="fbToolkitLink" onclick="window.scrollTo(0, body.scrollHeight)">Jump to bottom</a></li>
                             <li style="padding: 3px"><a href="#" class="fbToolkitLink" onclick="window.scrollTo(0, 0)">Jump to top</a></li>
@@ -218,4 +331,5 @@ init().then(() => {
     document.querySelector('a#fbToolkitScroll').addEventListener('click', () => scrollTimeline())
     document.querySelector('a#fbToolkitExpand').addEventListener('click', () => expandTimeline())
     document.querySelector('a#fbToolkitClear').addEventListener('click', () => clearTimeline())
+    document.querySelector('a#fbToolkitFriends').addEventListener('click', () => extractFriendlist())
 }).catch(exception => alertError(exception, 'Facebook Toolkit failed. Please reload page.'))
